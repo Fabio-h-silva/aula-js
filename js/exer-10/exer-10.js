74e2b6a9705806b6b5ad7d89582ec2cb040018ef
FABIO HENRIQUE DA SILVA <fabio.hsilva14@senacsp.edu.br>
2025-08-27 10:54:06 -0300

    exer-10

---
+++ js/exer-10/exer-10.js
@@ -0,0 +1,17 @@
+// Ex 9
+let opção = Number(prompt("Digite o número da opção desejada: \n 1 - Ver Saldo \n 2 - Depositar \n 3 - Sair"))
+
+
+switch (opção) {
+    case 1:
+        alert("O saldo da sua conta é de 50.000,00")
+        break;
+    case 2:
+        alert("Digite o valor a ser depositado")
+        break;
+    case 3:
+        alert("Voce esta saindo da sua conta")
+        break;
+
+}
+
