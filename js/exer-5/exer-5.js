658daae6ea2e72ef1f694e212c049ec2db9e418d
FABIO HENRIQUE DA SILVA <fabio.hsilva14@senacsp.edu.br>
2025-08-27 10:18:10 -0300

    exercico 5 e 6

--- js/exer-5/exer-5.js
+++ js/exer-5/exer-5.js
@@ -1,28 +1,23 @@
 // Ex 4
-let numero1 = number(prompt("Digite o primeiro numero:"))
+let numero1 = Number(prompt("Digite o primeiro numero:"))
 let operação = prompt("Digite uma operação (+,-,*,/)")
-let numero2 = number(prompt("Digite o segundo numero:"))
+let numero2 = Number(prompt("Digite o segundo numero:"))
 
 
-switch (operação) {
-    case ("+"):
-        total = numero1 + numero2
-        alert(`O resultado da soma é ${total}`)
-        break;
-    case ("-"):
-        total = numero1 - numero2
-        alert(`O resultado da subtração é ${total}`)
-        break;
-    case ("*"):
-        total = numero1 * numero2
-        alert(`O resultado da multiplicação é ${total}`)
-        break;
-    case ("/"):
-        total = numero1 / numero2
-        alert(`O resultado da divisão é ${total}`)
-        break;
-    
+if (operação == "+") {
+    total = numero1 + numero2
+    alert(`O resultado da soma é ${total}`)
+} else if (operação == "-") {
+    total = numero1 - numero2
+    alert(`O resultado da subtração é ${total}`)
+} else if (operação == "*") {
+    total = numero1 * numero2
+    alert(`O resultado da multiplicação é ${total}`)
+} else {
+    total = numero1 / numero2
+    alert(`O resultado da divisão é ${total}`)
 }
+
 
 
 
