54884f14c87446441f619a60e76e41e376689da7
FABIO HENRIQUE DA SILVA <fabio.hsilva14@senacsp.edu.br>
2025-08-27 11:15:08 -0300

    exer-11

---
+++ js/exer-11/exer-11.js
@@ -0,0 +1,12 @@
+// Ex 9
+let num_um = Number(prompt("Digite o primeiro número:"))
+let num_dois = Number(prompt("Digite o segundo número:"))
+
+
+if (num_um > num_dois) {
+    alert(`O primeiro número é o maior: ${num_um}`)
+} else if (num_um < num_dois) {
+    alert(`O segundo número é o maior: ${num_dois}`)
+} else {
+    alert(`O primeiro e segundo numero são iguais: ${num_um}`)
+}
