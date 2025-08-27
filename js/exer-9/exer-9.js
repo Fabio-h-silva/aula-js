afaaef0fddb74b28f2ac82ea488045b58ac8dba8
FABIO HENRIQUE DA SILVA <fabio.hsilva14@senacsp.edu.br>
2025-08-27 10:44:37 -0300

    exercício 9

---
+++ js/exer-9/exer-9.js
@@ -0,0 +1,12 @@
+// Ex 9
+let nota = Number(prompt("Digite a nota:"))
+
+
+if (nota >= 7) {
+    alert("Aprovado")
+} else if (nota == 5 && nota < 7) {
+    alert("Recuperação")
+} else {
+    alert("Reprovado")
+}
+
