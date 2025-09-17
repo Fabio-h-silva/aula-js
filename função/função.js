ec62d705445af7f18e9249d59386f3814f831f0f
Fabio-h-silva <fabioempresamsp@gmail.com>
2025-09-17 10:43:33 -0300

    funcao exemplo

--- função/função.js
+++ função/função.js
@@ -6,8 +6,9 @@ function Soma() {
     console.log(2 + 2)
 }
 
-
-Dobro(2, 2)
+let numUm = Number(prompt("digite primeiro número:"))
+let numDois = Number(prompt("digite segundo número:"))
+Dobro(numUm, numDois)
 function Dobro(numUm, numDois) {
     alert(numUm * numDois)
 
